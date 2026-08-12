68ef13110de558d964d5e23c48c15b650ac7b6d5
Nikita Mazurenko <mazurenko.nikita.mn@gmail.com>
2026-08-12 14:54:37 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,29 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    list_command = command.split()
+
+    if (
+        len(list_command) != 3
+        or list_command[0] != "mv"
+        or list_command[1] == list_command[2]
+    ):
+        return
+
+    source = list_command[1]
+    destination = list_command[2]
+
+    if destination.endswith("/"):
+        filename = os.path.basename(source)
+        destination = os.path.join(destination, filename)
+
+    directory = os.path.dirname(destination)
+
+    if directory != "":
+        os.makedirs(directory, exist_ok=True)
+
+    with open(source, "r") as file_in, open(destination, "w") as file_out:
+        file_out.write(file_in.read())
+
+    os.remove(source)
